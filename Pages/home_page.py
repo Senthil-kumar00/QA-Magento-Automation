6dc80c7b0050d7b618b5b4d18cac0c8e3b9593a0
Senthil <141906614+Senthil-kumar00@users.noreply.github.com>
2025-07-10 12:53:36 +0530

    Delete Pages directory

--- Pages/home_page.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver.common.by import By
-from pages.base_page import BasePage
-
-class HomePage(BasePage):
-    SIGN_OUT = (By.LINK_TEXT, "Sign Out")
-
-    def logout(self):
-        self.click(self.SIGN_OUT)
